3401e3d6a71025019973805c0cd82aab646ec042
ppoitier <hanofound@hotmail.com>
2020-08-29 10:48:08 +0200

    Set team description and idea optional

--- src/models/team.model.js
+++ src/models/team.model.js
@@ -24,8 +24,8 @@ module.exports = (sequelize, DataTypes) => {
             allowNull: true,
             unique: true,
             validate: {
-                notEmpty: true,
-                len: [3, 256]
+                notEmpty: false,
+                len: [0, 256]
             }
         },
         idea: {
@@ -33,8 +33,8 @@ module.exports = (sequelize, DataTypes) => {
             allowNull: true,
             unique: false,
             validate: {
-                notEmpty: true,
-                len: [3, 256]
+                notEmpty: false,
+                len: [0, 256]
             }
         },
         token: {
